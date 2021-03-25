9dfac76241214b0729bdf1e3ac07e23215a64cf3
Windchester <litelpg@mail.ru>
2021-03-25 04:54:57 +0200

    add mixins, update css-reset, add basic html and js

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+// vanilla
+const site = {
+  init() {
+    
+    console.log('site.init()');
+  }
+}
+
+(() => {
+  site.init()
+})()
+
+
+// jQuery
+
+// $.site = {
+//   init() {
+
+//     console.log('$.site.init()');
+//   }
+// }
+
+// jQuery(() => {
+//   $.site.init()
+// })
